f403dca33eee92835afcf269b15763cb0b165655
Sourav69 <souravkumarjha301@gmail.com>
2024-06-29 15:07:32 +0530

    I've done the basics of First react App

---
+++ React-JS/harry-react/new-project/src/App.jsx
@@ -0,0 +1,26 @@
+// import react from "react";
+
+function App() {
+
+  let name = "Sourav kumar jha";
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello may name is {name}</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere,
+          voluptatem, reiciendis cumque dolor, odio esse nesciunt eos
+          reprehenderit praesentium ratione voluptas earum eligendi aspernatur?
+          Voluptates laboriosam ab nam consectetur necessitatibus!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
