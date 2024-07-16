56d469e5b32a253718ebea5226f8ee1f89f60f3c
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-07-16 09:41:15 +0530

    Last commit

--- Tailwind-CSS/Lecture 2/src/components/BracketNotation.jsx
+++ Tailwind-CSS/Lecture 2/src/components/BracketNotation.jsx
@@ -1,7 +1,7 @@
 function Notation() {
   return (
     <>
-    {/* This is Sqauare bracket Notation */}
+      {/* This is Sqauare bracket Notation */}
       <div className="bg-red-500  mx-4 w-[40vw]">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab assumenda
         quibusdam tenetur impedit iure inventore error aspernatur, voluptates
